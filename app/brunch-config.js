6438b20245bf9c2ed9775c22cf74dbe2527780b4
Frank Panetta <frank.panetta@savvi.io>
2017-07-08 23:49:22 +1000

    Example

---
+++ app/brunch-config.js
@@ -0,0 +1,56 @@
+exports.config = {
+  // See http://brunch.io/#documentation for docs.
+  files: {
+    javascripts: {
+    joinTo: "js/app.js"
+    // To use a separate vendor.js bundle, specify two files path
+    // https://github.com/brunch/brunch/blob/master/docs/config.md#files
+    // joinTo: {
+    //  "js/app.js": /^(js)/,
+    //  "js/vendor.js": /^(vendor)|(deps)/
+    // }
+    //
+    // To change the order of concatenation of files, explicitly mention here
+    // https://github.com/brunch/brunch/tree/master/docs#concatenation
+    // order: {
+    //   before: [
+    //     "vendor/js/jquery-2.1.1.js",
+    //     "vendor/js/bootstrap.min.js"
+    //   ]
+    // }
+    },
+    stylesheets: {
+      joinTo: "css/app.css"
+    }
+  },
+  conventions: {
+  // This option sets where we should place non-css and non-js assets in.
+  // By default, we set this to "/assets/static". Files in this directory
+  // will be copied to `paths.public`, which is set below to "../public".
+    assets: /^(static)/
+  },
+  // paths configuration
+  paths: {
+    // Dependencies and current project directories to watch
+    watched: ["static", "css", "js", "vendor", "elm"],
+    // Where to compile files to
+    public: "../public"
+  },
+  // Configure your plugins
+  plugins: {
+    babel: {
+      // Do not use ES6 compiler in vendor code
+      ignore: [/vendor/]
+    },
+    elmBrunch: {
+      elmFolder: "elm",
+      mainModules: ["Main.elm"],
+      outputFolder: "../js"
+    },
+  },
+  modules: {
+    autoRequire: {
+      "js/app.js": ["js/app"]
+    }
+  }
+};
